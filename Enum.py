2256233ce64746fd97663e749065c1e4ea708994
Tundzhay Mollov <graculapsone1998@gmail.com>
2019-07-07 21:10:38 +0300

    Now @ Enum.py almost every constant uses StringFore class for generating colourful strings.

--- Enum.py
+++ Enum.py
@@ -11,21 +11,29 @@ class StringFore:
     
     def RED(content):
         return F"{Fore.RED}{content}{Fore.RED}{Fore.WHITE}"
+
+    def YELLOW(content):
+        return F"{Fore.YELLOW}{content}{Fore.YELLOW}{Fore.WHITE}"    
     
     def CYAN(content):
         return F"{Fore.CYAN}{content}{Fore.CYAN}{Fore.WHITE}"
+    
+    def LIGHTMAGENTA_EX(content):
+        return F"{Fore.LIGHTMAGENTA_EX}{content}{Fore.LIGHTMAGENTA_EX}{Fore.WHITE}"
+    
+    def LIGHTBLUE_EX(content):
+        return F"{Fore.LIGHTBLUE_EX}{content}{Fore.LIGHTBLUE_EX}{Fore.WHITE}"
 
 SearchByName = 0
 SearchByCity = 1
 SearchByNumber = 2
-SearchName = "Enter Name: "
-SearchCity = "Enter City: "
-SearchNumber = "Enter Phone Number: "
 
 Enter = "enter"
 UpArrow = "up"
 DownArrow = "down"
-EnterForMainMenu = f"Press {Fore.LIGHTBLUE_EX}ENTER{Fore.LIGHTBLUE_EX}{Fore.WHITE} to return to main menu..."
+
+BlueEnter = StringFore.LIGHTBLUE_EX("ENTER")
+EnterForMainMenu = f"Press {BlueEnter} to return to main menu..."
 
 NewLine = "\n"
 Tab = "\t"
@@ -39,21 +47,24 @@ NameAndCityInfo = "Name and City must contain only lower/upper case alphabetic c
 EnterName = "Enter Name: "
 EnterCity = "Enter City: "
 EnterPhoneNumber = "Enter Phone Number: "
-SearchBy = "Search by ..."
 ContactAdded = StringFore.GREEN("*** Contact Added! ***")
-ContactUpdated = StringFore.GREEN("*** Contact Updated! ***")
-DeleteInfo = f"{Fore.LIGHTMAGENTA_EX}Select contact to delete...{Fore.LIGHTMAGENTA_EX}{Fore.WHITE}"
 
-UpdateContactInfo = f"{Fore.LIGHTMAGENTA_EX}Select contact to update...{Fore.LIGHTMAGENTA_EX}{Fore.WHITE}"
+# Delete Contact strings
+DeleteInfo = StringFore.LIGHTMAGENTA_EX("Select contact to delete...")
+ContactDeleted = StringFore.GREEN("*** Contact Deleted! ***")
+ContactNotDeleted = StringFore.LIGHTCYAN_EX("Contact not deleted.")
+
+# Update Contact strings
+UpdateContactInfo = StringFore.LIGHTMAGENTA_EX("Select contact to update...")
+ContactUpdated = StringFore.GREEN("*** Contact Updated! ***")
+
+# Search By strings
+SearchBy = "Search by ..."
+SearchOptions = ["By Name", "By City", "By Phone Number"]
+SearchOptionsClear = ["Name" , "City", "Phone Number"]
 
 MenuOptions = ["Search by...", "All Contacts",
                   "Add Contact", "Delete Contact", "Edit Contact"]
-
-SearchOptions = ["By Name", "By City", "By Phone Number"]
-SearchOptionsClear = ["Name" , "City", "Phone Number"]
-ContactNotDeleted = f"{Fore.LIGHTCYAN_EX}Contact not deleted.{Fore.LIGHTCYAN_EX}{Fore.WHITE}"
-
-
 
 def EnterNameUpdate(name):
     return f"Name from {name} to : "
@@ -66,14 +77,17 @@ def EnterNumberUpdate(number):
 
 #### INVALID INPUT
 InvalidName = f"Name must contain only lower, upper or mixed case alphabetic character and spaces!"
-InvNameLen = f"Name length must be between 2-50 charachters!"
+InvNameLen = f"Name length must be between 3-50 charachters!"
 
 InvalidCity = f"City must contain only lower, upper or mixed case alphabetic character and spaces!"
-InvNameLen = f"City length must be between 2-50 charachters!"
+InvNameLen = f"City length must be between 3-50 charachters!"
 
 InvalidPhone = f"Phone number must contains only digits!"
 InvPhoneLen = f"Phone number must be between 5-15"
-InvalidComfirmation = f"Please type {Fore.LIGHTBLUE_EX}y / yes{Fore.LIGHTBLUE_EX}{Fore.WHITE} or {Fore.LIGHTBLUE_EX}n / no{Fore.LIGHTBLUE_EX}{Fore.WHITE}"
+
+yesOption = StringFore.LIGHTBLUE_EX("y / yes")
+noOption = StringFore.LIGHTBLUE_EX("n / no")
+InvalidComfirmation = f"Please type {yesOption} or {noOption}"
 #### Exceptions
 def ContactExist(contactName,contactCity,contactNumber):
- return f"{Fore.YELLOW}Contact: {contactNumber} - {contactName}|{contactCity} already exists!{Fore.YELLOW}{Fore.WHITE}"
+ return StringFore.YELLOW(f"Contact: {contactNumber} - {contactName}|{contactCity} already exists!")
